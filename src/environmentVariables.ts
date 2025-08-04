89783631fff75094022c7eff6d9cda0d4b2436cd
Master1624 <jdpa83@gmail.com>
2025-08-04 12:53:11 -0500

    chore: Add environmentVariables file and edit .gitignore file

---
+++ src/environmentVariables.ts
@@ -0,0 +1,7 @@
+export const environmentVariables = {
+  personalInformation: {
+    phoneNumber: process.env.REACT_APP_PHONE_NUMBER || "",
+    email: process.env.REACT_APP_EMAIL || "",
+    linkedinProfile: process.env.REACT_APP_LINKEDIN_PROFILE || "",
+  },
+};
