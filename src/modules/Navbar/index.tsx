8837d673b94a3df010d93a2744ce13a5f3d3e070
Master1624 <jdpa83@gmail.com>
2025-08-05 13:09:02 -0500

    refactor: Update styles in components

--- src/modules/Navbar/index.tsx
+++ src/modules/Navbar/index.tsx
@@ -1,20 +1,21 @@
 import { Link } from "react-router-dom";
 import { links } from "./const";
-import { Button } from "@mui/material";
-import styles from "./Navbar.module.scss"
+import { Button, Card, CardContent } from "@mui/material";
+import styles from "./Navbar.module.scss";
 
 function Navbar() {
   return (
-    <div className={styles["navigation-container"]}>
-      {links.map((link) => (
-        <Link
-          key={link.name}
-          to={link.linkPath}
-        >
-          <Button>{link.name}</Button>
-        </Link>
-      ))}
-    </div>
+    <Card>
+      <CardContent className={styles["navigation-container"]}>
+        {links.map((link) => (
+          <Link key={link.name} to={link.linkPath}>
+            <Button color="info" className={styles["navigation-button"]}>
+              {link.name}
+            </Button>
+          </Link>
+        ))}
+      </CardContent>
+    </Card>
   );
 }
 
